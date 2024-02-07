31d31ee4264c78312c25ce8c41d73797d3744330
Pascal <pascal.rieder@outlook.com>
2024-02-07 16:23:17 +0100

    Update nuxt config
    Body has now tailwind classes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
+  app: {
     head: {
-      
-      script: [{ src: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js' }],
+      title: "About",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
+      script: [
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js",
+        },
+      ],
+      bodyAttrs: {
+        class:
+          "antialiased bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100",
+      },
     },
   },
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
-})
+  modules: ["@nuxtjs/tailwindcss"],
+});
