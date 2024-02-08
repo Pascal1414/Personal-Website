0754a2b0129f1d5e8f3c58a3167c85f6e45ccf04
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-02-08 13:16:56 +0100

    Update Header

--- components/Header.vue
+++ components/Header.vue
@@ -53,8 +53,8 @@ export default defineComponent({
                 aria-current="page">About</a>
             </li>
             <li>
-              <a href="#"
-                class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+              <a href="#repositories"
+                class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Repositories</a>
             </li>
             <li>
               <a href="#"
