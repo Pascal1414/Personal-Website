f15237a0db19118a398b32f05e7f3da6f925b7ff
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-02-09 12:01:05 +0100

    Update Header

--- components/Header.vue
+++ components/Header.vue
@@ -7,7 +7,7 @@
           <img src="/favicon.svg" class="h-8" alt="Flowbite Logo" />
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Pascal</span>
         </a>
-        <ThemeToggleButton/>
+        <ThemeToggleButton />
         <button data-collapse-toggle="navbar-solid-bg" type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-solid-bg" aria-expanded="false">
@@ -43,3 +43,21 @@
     </nav>
   </header>
 </template>
+<style scoped>
+#navbar {
+  opacity: 0;
+  transform: translateY(-100%);
+  transition: all 1s;
+}
+
+.navbar-visible {
+  opacity: 1 !important;
+  transform: translateY(0) !important;
+}
+</style>
+<script setup>
+onMounted(() => {
+  document.getElementById('navbar').classList.add('navbar-visible');
+
+});
+</script>
