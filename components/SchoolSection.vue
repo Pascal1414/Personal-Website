21b9cb00825dfd7f658178b27a738bab33449f71
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-02-12 12:32:24 +0100

    Update SchoolSection

--- components/SchoolSection.vue
+++ components/SchoolSection.vue
@@ -2,34 +2,43 @@
 <template>
   <section id="school-section">
     <div id="school" class="w-[90%] md:w-[85%] lg:w-[70%] ml-[auto] mr-[auto] z-[-10]" :style="{ height: sectionHeight }">
-      <div class="scroll-hidden py-8  mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
-        <div class="flex flex-col justify-center">
-          <h1 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
-            School</h1>
-          <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">While I am working a go to
-            school
-          </p>
 
+      <div class="scroll-hidden py-8  mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
+
+        <div class="left">
+          <div class="flex flex-col justify-center school-title-container">
+            <h1 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
+              School</h1>
+            <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">While I am working a go to
+              school
+            </p>
+
+          </div>
         </div>
 
-        <div class="flex flex-col overflow-hidden  gap-5">
-          <!-- Tbz -->
-          <div class="lg:max-w-xl p-6  sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
-            <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Technische Berufsschule Zürich
-            </h2>
-            <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Technische Berufsschule Zürich (TBZ) is a
-              vocational school in Zurich, Switzerland. It is the largest vocational school in the canton of Zurich and
-              offers a wide range of vocational training programs.</p>
-          </div>
 
-          <!-- BMZ -->
-          <div class="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
-            <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Berufsmaturitätsschule Zürich
-            </h2>
-            <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur
-              adipisicing elit. Vitae dicta earum deserunt perferendis, voluptates, placeat repellat sed reprehenderit
-              perspiciatis praesentium </p>
+        <div class="right">
 
+          <div class="flex flex-col overflow-hidden  gap-5 school-container">
+            <!-- Tbz -->
+            <div class="lg:max-w-xl p-6  sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800 school">
+              <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Technische Berufsschule Zürich
+              </h2>
+              <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Technische Berufsschule Zürich (TBZ) is
+                a
+                vocational school in Zurich, Switzerland. It is the largest vocational school in the canton of Zurich and
+                offers a wide range of vocational training programs.</p>
+            </div>
+
+            <!-- BMZ -->
+            <div class="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
+              <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Berufsmaturitätsschule Zürich
+              </h2>
+              <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Vitae dicta earum deserunt perferendis, voluptates, placeat repellat sed reprehenderit
+                perspiciatis praesentium </p>
+
+            </div>
           </div>
         </div>
       </div>
@@ -38,6 +47,37 @@
 </template>
 
 <style scoped>
+/* Scroll Animation */
+.left {
+  margin-left: 10px;
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+}
+
+.right {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.school-title-container {
+  position: sticky;
+  top: 100px;
+  width: 220px;
+  height: 320px;
+}
+
+.school-container {
+  height: 800px;
+}
+
+.school {
+  margin-bottom: 200px;
+}
+
+/* Parallax Effect */
 #school {
   position: absolute;
 }
