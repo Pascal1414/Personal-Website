3a8ce6db325e2a3d39c2ed80c2dc769755739c23
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-22 12:55:08 +0200

    Update TechnologiesSection

--- components/TechnologiesSection.vue
+++ components/TechnologiesSection.vue
@@ -9,8 +9,8 @@
       <p
         class="mb-10 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-center lg:text-xl lg:px-64 lg:mb-16"
       >
-        Wärend meiner Ausbildung habe ich mit verschiedenen Technologien und Entwicklungsumgebungen
-        gearbeitet. Mit den folgenden Technologien habe ich die meisten Erfahrungen gesammelt.
+        Wärend meiner Ausbildung habe ich mit verschiedenen Technologien und Programmen gearbeitet.
+        Mit den folgenden Technologien habe ich die meisten Erfahrungen gesammelt.
       </p>
       <div class="mt-12">
         <h2 class="text-gray-900 dark:text-gray-200 text-xl font-extrabold mb-2.5">
