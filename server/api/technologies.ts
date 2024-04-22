293780efa09088b574360683c89bdd199e31e706
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-22 11:58:57 +0200

    Update Technology.ts

--- server/api/technologies.ts
+++ server/api/technologies.ts
@@ -1,4 +1,4 @@
-import { Technology } from '~/types/Technology'
+import { TechType, Technology } from '~/types/Technology'
 
 export default defineEventHandler((event) => {
   // Return a list of technologies
@@ -6,17 +6,20 @@ export default defineEventHandler((event) => {
     {
       title: 'React',
       description: 'A JavaScript library for building user interfaces',
-      icon: '/technology/react.svg'
+      icon: '/technology/react.svg',
+      type: TechType.ProgrammingLanguage
     },
     {
       title: 'Nuxt.js',
       description: 'The Progressive JavaScript Framework',
-      icon: '/technology/nuxt.svg'
+      icon: '/technology/nuxt.svg',
+      type: TechType.IDE
     },
     {
       title: 'Angular',
       description: 'A platform and framework for building single-page client applications',
-      icon: '/technology/react.svg'
+      icon: '/technology/react.svg',
+      type: TechType.ProgrammingLanguage
     }
   ]
   return technologies
--- types/Technology.ts
+++ types/Technology.ts
@@ -1,5 +1,10 @@
+export enum TechType {
+  ProgrammingLanguage,
+  IDE
+}
 export interface Technology {
   title: string
   description: string
   icon: string
+  type: TechType
 }
