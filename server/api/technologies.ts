ceb249d3f03a0ca5da740d6d813750ad515eaf4c
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-22 12:02:48 +0200

    Update technologies.ts

--- server/api/technologies.ts
+++ server/api/technologies.ts
@@ -4,22 +4,70 @@ export default defineEventHandler((event) => {
   // Return a list of technologies
   const technologies: Technology[] = [
     {
-      title: 'React',
-      description: 'A JavaScript library for building user interfaces',
+      title: 'C#',
+      description: 'Description',
+      icon: '/technology/react.svg',
+      type: TechType.ProgrammingLanguage
+    },
+    {
+      title: 'Kotlin',
+      description: 'Description',
+      icon: '/technology/nuxt.svg',
+      type: TechType.ProgrammingLanguage
+    },
+    {
+      title: 'Java',
+      description: 'Description',
+      icon: '/technology/react.svg',
+      type: TechType.ProgrammingLanguage
+    },
+    {
+      title: 'JavaScript',
+      description: 'Description',
+      icon: '/technology/react.svg',
+      type: TechType.ProgrammingLanguage
+    },
+    {
+      title: 'Vue.js',
+      description: 'Description',
       icon: '/technology/react.svg',
       type: TechType.ProgrammingLanguage
     },
     {
       title: 'Nuxt.js',
-      description: 'The Progressive JavaScript Framework',
-      icon: '/technology/nuxt.svg',
+      description: 'Description',
+      icon: '/technology/react.svg',
+      type: TechType.ProgrammingLanguage
+    },
+    {
+      title: 'Android Studio',
+      description: 'Description',
+      icon: '/technology/react.svg',
       type: TechType.IDE
     },
     {
-      title: 'Angular',
-      description: 'A platform and framework for building single-page client applications',
+      title: 'Visual Studio',
+      description: 'Description',
       icon: '/technology/react.svg',
-      type: TechType.ProgrammingLanguage
+      type: TechType.IDE
+    },
+    {
+      title: 'Visual Studio Code',
+      description: 'Description',
+      icon: '/technology/react.svg',
+      type: TechType.IDE
+    },
+    {
+      title: 'Rider',
+      description: 'Description',
+      icon: '/technology/react.svg',
+      type: TechType.IDE
+    },
+    {
+      title: 'IntelliJ IDEA',
+      description: 'Description',
+      icon: '/technology/react.svg',
+      type: TechType.IDE
     }
   ]
   return technologies
