fef58746ecd0362fc238b72e1764513b09eafb65
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-22 10:21:48 +0200

    Update css

--- components/TechnologiesSection.vue
+++ components/TechnologiesSection.vue
@@ -1,13 +1,20 @@
 <template>
-  <section class="bg-gray-50 dark:bg-gray-800 border-t border-b border-gray-100 dark:border-gray-700">
-    <div class=" max-w-screen-xl px-4 py-8 mx-auto lg:py-24">
+  <section class="">
+    <div class="max-w-screen-xl px-4 py-8 mx-auto lg:py-24">
       <h2
-        class="mb-6 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 lg:text-center dark:text-white md:text-4xl">
-        Flowbite in other technologies</h2>
-      <p class="mb-10 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-center lg:text-xl lg:px-64 lg:mb-16">
-        You can now also get started with open-source component libraries built for frameworks such as React, Vue.js,
-        Svelte and Angular.</p>
-      <div class="space-y-4 sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-4 xl:gap-8 sm:space-y-0 md:mt-12">
+        class="mb-6 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 lg:text-center dark:text-white md:text-4xl"
+      >
+        Flowbite in other technologies
+      </h2>
+      <p
+        class="mb-10 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-center lg:text-xl lg:px-64 lg:mb-16"
+      >
+        You can now also get started with open-source component libraries built for frameworks such
+        as React, Vue.js, Svelte and Angular.
+      </p>
+      <div
+        class="space-y-4 sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-4 xl:gap-8 sm:space-y-0 md:mt-12"
+      >
         <Technology />
         <Technology />
         <Technology />
@@ -19,8 +26,6 @@
   </section>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <style></style>
