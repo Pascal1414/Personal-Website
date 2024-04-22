b684ccf4df62234ca42c6f9e187a7496c3bf8c4f
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-22 18:51:06 +0200

    Update TechnologiesSection

--- components/TechnologiesSection.vue
+++ components/TechnologiesSection.vue
@@ -13,22 +13,24 @@
             gearbeitet. Mit den folgenden Technologien habe ich die meisten Erfahrungen gesammelt.
           </p>
         </div>
-        <svg
-          class="mr-12 hidden md:block"
-          width="425"
-          height="250"
-          viewBox="0 0 794 453"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M180.33 134.172L47 238.878L180.33 361.034M613.67 134.172L747 238.878L613.67 361.034M509 48L400 405"
-            stroke="#1c64f2"
-            stroke-width="94"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
+        <ClientOnly>
+          <svg
+            class="mr-12 hidden md:block"
+            width="425"
+            height="250"
+            viewBox="0 0 794 453"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M180.33 134.172L47 238.878L180.33 361.034M613.67 134.172L747 238.878L613.67 361.034M509 48L400 405"
+              stroke="#1c64f2"
+              stroke-width="94"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </ClientOnly>
       </div>
       <div class="mb-12">
         <h2 class="text-gray-900 dark:text-gray-200 text-xl font-extrabold mb-2.5">
