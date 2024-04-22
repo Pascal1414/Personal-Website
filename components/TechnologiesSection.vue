4ee09cd8839a542918e06ec5a4e812548586cab3
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-22 12:32:24 +0200

    Update TechnologiesSection

--- components/TechnologiesSection.vue
+++ components/TechnologiesSection.vue
@@ -9,21 +9,43 @@
       <p
         class="mb-10 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-center lg:text-xl lg:px-64 lg:mb-16"
       >
-        Wärend meiner Ausbildung habe ich mit verschiedenen Technologien gearbeitet. Mit den
-        folgenden Technologien habe ich die meisten Erfahrungen gesammelt.
+        Wärend meiner Ausbildung habe ich mit verschiedenen Technologien und Entwicklungsumgebungen
+        gearbeitet. Mit den folgenden Technologien habe ich die meisten Erfahrungen gesammelt.
       </p>
-      <div
-        class="space-y-4 sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-4 xl:gap-8 sm:space-y-0 md:mt-12"
-      >
-        <Technology v-for="technology in technologies" :technology="technology" />
+      <div class="md:mt-12">
+        <h2 class="text-gray-900 dark:text-gray-200 text-xl font-extrabold mb-2.5">
+          Programmiersprachen und Frameworks
+        </h2>
+        <div class="space-y-4 sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-4 xl:gap-8 sm:space-y-0">
+          <Technology v-for="technology in programmingLanguages" :technology="technology" />
+        </div>
+      </div>
+      <div class="md:mt-12">
+        <h2 class="text-gray-900 dark:text-gray-200 text-xl font-extrabold mb-2.5">
+          Entwicklungsumgebungen
+        </h2>
+        <div class="space-y-4 sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-4 xl:gap-8 sm:space-y-0">
+          <Technology v-for="technology in ides" :technology="technology" />
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script lang="ts" setup>
-const { data: technologies } = await useFetch('/api/technologies')
-console.log(technologies)
+import { TechType, type Technology } from '~/types/Technology'
+
+const programmingLanguages: Ref<Technology[]> = ref([])
+const ides: Ref<Technology[]> = ref([])
+
+await useFetch('/api/technologies', {
+  transform: (data) => {
+    programmingLanguages.value = data.filter(
+      (technology: Technology) => technology.type === TechType.ProgrammingLanguage
+    )
+    ides.value = data.filter((technology: Technology) => technology.type === TechType.IDE)
+  }
+})
 </script>
 
 <style></style>
