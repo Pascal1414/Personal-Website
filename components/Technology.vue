b7cecaac3123ad50f448918f5616c5d8a75f3b4c
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-23 09:34:14 +0200

    Update Technology

--- components/Technology.vue
+++ components/Technology.vue
@@ -1,13 +1,12 @@
 <template>
-  <a
-    href="#"
+  <div
     class="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
   >
     <img :src="technology.icon" alt="technology.title" class="w-12 h-12 mx-auto" />
     <h3 class="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
       {{ technology.title }}
     </h3>
-  </a>
+  </div>
 </template>
 
 <script lang="ts" setup>
