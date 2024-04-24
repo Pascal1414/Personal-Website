81683ac9ed99b09a33ea5fa98ca7607862a3772e
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-24 10:42:43 +0200

    Update Technology

--- components/Technology.vue
+++ components/Technology.vue
@@ -22,7 +22,14 @@
         <div
           class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
         >
-          <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Terms of Service</h3>
+          <div class="flex items-center gap-5">
+            <img :src="technology.icon" alt="technology.title" class="w-9 h-9 mx-auto" />
+
+            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+              {{ technology.title }}
+            </h3>
+          </div>
+
           <button
             @click="closeModal"
             type="button"
@@ -50,35 +57,22 @@
         <!-- Modal body -->
         <div class="p-4 md:p-5 space-y-4">
           <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-            With less than a month to go before the European Union enacts new consumer privacy laws
-            for its citizens, companies around the world are updating their terms of service
-            agreements to comply.
-          </p>
-          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on
-            May 25 and is meant to ensure a common set of data rights in the European Union. It
-            requires organizations to notify users as soon as possible of high-risk data breaches
-            that could personally affect them.
+            {{ technology.description }}
           </p>
         </div>
         <!-- Modal footer -->
         <div
           class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"
         >
-          <button
-            data-modal-hide="default-modal"
-            type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          <a
+            v-bind:href="technology.link"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            I accept
-          </button>
-          <button
-            data-modal-hide="default-modal"
-            type="button"
-            class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-          >
-            Decline
-          </button>
+            Go to website
+            <Icon name="material-symbols:link-rounded" class="w-5 h-5 ms-2 rtl:rotate-180" />
+          </a>
         </div>
       </div>
     </div>
