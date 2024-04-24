b9c52931f7b11500e923c4cf598f9569280e673c
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-24 11:23:50 +0200

    Update RepositorySection

--- components/RepositorySection.vue
+++ components/RepositorySection.vue
@@ -49,7 +49,7 @@
               rel="noopener noreferrer"
               class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              See on Github
+              Github
               <ClientOnly>
                 <svg
                   class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
@@ -75,7 +75,7 @@
               rel="noopener noreferrer"
               class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Open Website
+              Webseite
               <Icon name="material-symbols:link-rounded" class="w-5 h-5 ms-2 rtl:rotate-180" />
             </a>
           </div>
