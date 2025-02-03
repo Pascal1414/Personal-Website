1b9de79c4050e1e3b4ac9b491f4aac9a0bad5a71
Pascal <pascal.rieder@outlook.com>
2025-02-03 15:17:13 +0100

    Update ArtificialIntelligence.vue

--- components/ArtificialIntelligence.vue
+++ components/ArtificialIntelligence.vue
@@ -5,28 +5,25 @@
     <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Arbeiten mit KI</h5>
     <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
       Auch wenn ich gerne ohne KI-Hilfen programmiere, ist es für mich dennoch wichtig, solche Tools
-      zu nutzen, um immer auf dem neuesten Stand und wettbewerbsfähig zu bleiben. In der heutigen,
-      schnelllebigen Programmierwelt ist es für Entwickler entscheidend, ihre Arbeit mit KI-Tools zu
-      unterstützen. Besonders bei wiederholenden Aufgaben oder bei der Suche nach Lösungen für
-      Probleme greife ich auf KI-Tools zurück.
+      zu effizient nutzen zu können. In der heutigen, schnelllebigen Programmierwelt ist es für
+      Entwickler wichtig, sich ständig auf neue Technologien einzulassen. Besonders bei sich
+      wiederholenden Aufgaben oder bei der Suche nach Lösungen für Probleme greife ich auf KI-Tools
+      zurück.
     </p>
     <div
       class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse"
     >
       <div
         v-for="tool in tools"
-        class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
+        class="w-full sm:w-auto bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
       >
-        <a href="#">
-          <img class="rounded-t-lg" src="" alt="" />
-        </a>
-
         <div class="p-5">
-          <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {{ tool.title }}
-            </h5>
-          </a>
+          <div class="flex justify-center">
+            <img class="rounded-t-lg" :src="tool.image" alt="" />
+          </div>
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {{ tool.title }}
+          </h5>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {{ tool.description }}
           </p>
