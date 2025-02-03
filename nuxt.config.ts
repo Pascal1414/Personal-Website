0089ac8246d3400faedbfca668e529e2cd6f3032
Pascal <pascal.rieder@outlook.com>
2025-02-03 12:27:39 +0100

    Refactor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,25 +2,25 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "About",
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
+      title: 'About',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
       script: [
         {
-          src: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js",
-        },
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js'
+        }
       ],
       bodyAttrs: {
         class:
-          "antialiased bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100 trainsition-all duration-1000",
+          'antialiased bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100 trainsition-all duration-1000'
       },
       htmlAttrs: {
-        class: "scroll-smooth",
-      },
-    },
+        class: 'scroll-smooth'
+      }
+    }
   },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "nuxt-icon"],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', 'nuxt-icon'],
   colorMode: {
-    classSuffix: "",
-  },
-});
+    classSuffix: ''
+  }
+})
