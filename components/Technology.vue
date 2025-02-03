a1d863e09724515775e9b220a849f75a2044ad18
Pascal <pascal.rieder@outlook.com>
2025-02-03 15:21:06 +0100

    Update Technology

--- components/Technology.vue
+++ components/Technology.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click="openModal"
-    class="w-full block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
+    class="w-full block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
   >
     <img :src="technology.icon" alt="technology.title" class="w-12 h-12 mx-auto" />
     <h3 class="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
