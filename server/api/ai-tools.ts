b8997fc34a28c3301b501e23909340aaef338e11
Pascal <pascal.rieder@outlook.com>
2025-02-03 15:18:04 +0100

    Update ai-tools api

--- server/api/ai-tools.ts
+++ server/api/ai-tools.ts
@@ -3,16 +3,18 @@ import type { AiTool } from '~/types/AiTool'
 export default defineEventHandler(async () => {
   const technologies: AiTool[] = [
     {
-      image: 'testimage',
-      title: 'testtitle',
-      description: 'testdesc',
-      link: 'testlink'
+      image: '/ai-tools/clipboard.svg',
+      title: 'Github Copilot',
+      description:
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
+      link: 'https://github.com/features/copilot'
     },
     {
-      image: 'testimage',
-      title: 'testtitle',
-      description: 'testdesc',
-      link: 'testlink'
+      image: '/ai-tools/message.svg',
+      title: 'ChatGPT',
+      description:
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
+      link: 'https://openai.com/chatgpt/overview/'
     }
   ]
   return technologies
