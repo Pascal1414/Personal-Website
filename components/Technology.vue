56d1bc0fd8adf6faf1969b11f39333dfda37cf46
Pascal <pascal.rieder@outlook.com>
2025-02-05 19:03:48 +0100

    Update Technology

--- components/Technology.vue
+++ components/Technology.vue
@@ -1,22 +1,42 @@
 <template>
-  <button
-    @click="openModal"
-    class="w-full block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
-  >
-    <img :src="technology.icon" alt="technology.title" class="w-12 h-12 mx-auto" />
-    <h3 class="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-      {{ technology.title }}
-    </h3>
+  <button @click="openModal">
+    <div
+      class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 text-start"
+    >
+      <svg
+        class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"
+        />
+      </svg>
+      <a href="#">
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          {{ technology.title }}
+        </h5>
+      </a>
+      <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+        A typed superset of JavaScript that compiles to plain JavaScript.
+      </p>
+    </div>
   </button>
-
   <!-- Main modal -->
-  <div ref="modal" aria-hidden="true"
-    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+  <div
+    ref="modal"
+    aria-hidden="true"
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+  >
     <div class="relative p-4 w-full max-w-2xl max-h-full">
       <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
         <!-- Modal header -->
-        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+        <div
+          class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
+        >
           <div class="flex items-center gap-5">
             <img :src="technology.icon" alt="technology.title" class="w-9 h-9 mx-auto" />
 
@@ -25,12 +45,26 @@
             </h3>
           </div>
 
-          <button @click="closeModal" type="button"
+          <button
+            @click="closeModal"
+            type="button"
             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            data-modal-hide="default-modal">
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            data-modal-hide="default-modal"
+          >
+            <svg
+              class="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
             </svg>
             <span class="sr-only">Close modal</span>
           </button>
@@ -42,10 +76,16 @@
           </p>
         </div>
         <!-- Modal footer -->
-        <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-          <a v-bind:href="technology.link" target="_blank" rel="noopener noreferrer"
+        <div
+          class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"
+        >
+          <a
+            v-bind:href="technology.link"
+            target="_blank"
+            rel="noopener noreferrer"
             class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            v-if="technology.link">
+            v-if="technology.link"
+          >
             Website öffnen
             <Icon name="material-symbols:link-rounded" class="w-5 h-5 ms-2 rtl:rotate-180" />
           </a>
