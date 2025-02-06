b42c71276980b1f8c1d31a909ea31500a728cb8c
Pascal <pascal.rieder@outlook.com>
2025-02-06 10:16:07 +0100

    Update Technology

--- components/Technology.vue
+++ components/Technology.vue
@@ -9,28 +9,9 @@
       >
         <img class="w-[28px] h-[28px]" :src="technology.icon" />
       </div>
-      <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+      <h5 class="text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
         {{ technology.title }}
       </h5>
-
-      <div class="inline-flex font-medium items-center text-blue-600 hover:underline mt-auto">
-        Mehr erfahren
-        <svg
-          class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          />
-        </svg>
-      </div>
     </div>
   </button>
 
